05a649be627bc7b1a8008ff9416fc53c8652cc50
Pablo Giordano <pablogiordanog@gmail.com>
2023-12-11 22:20:14 -0300

    Proyecto Base Con Menu parcial

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx"
+import Home from './components/Home.jsx';
+import Footer from "./components/Footer.jsx"
+import About from "./components/About.jsx"
+import IndexShop from "./components/shop/IndexShop.jsx"
+import IndexStories from "./components/stories/IndexStories.jsx"
+import Search from "./components/Search.jsx"
+
+import Login from "./components/Login.jsx"
+import Page404 from "./components/Page404.jsx"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/indexshop" element={<IndexShop />} />
+          <Route path="/indexstories" element={<IndexStories />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>      
+    </div>
   )
 }
 
