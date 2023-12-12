71bd5f7a6c4fdde364553352be07bd8d5f833281
Pablo Giordano <pablogiordanog@gmail.com>
2023-12-12 07:30:48 -0300

    Actualizacion de Archivos de Componente 22

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/ecommerce2023/",
 })
 //base:"/ecommerce2023/",
 //base:"https://pablogiordanog.github.io/ecommerce2023/",
